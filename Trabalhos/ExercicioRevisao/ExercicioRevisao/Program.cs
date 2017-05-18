c0f1cba08af36550a8983cc1204cd9eab2ac3c2f
jairo151099 <jairojunior10410@gmail.com>
2017-05-18 13:48:28 -0300

    Etapa 3: Uso de subrotinas

--- Trabalhos/ExercicioRevisao/ExercicioRevisao/Program.cs
+++ Trabalhos/ExercicioRevisao/ExercicioRevisao/Program.cs
@@ -9,42 +9,53 @@ namespace ExercicioRevisao
     {
         static void Main(string[] args)
         {
-            int n;
+            int i, n, pot;
             string modelo;
-            double km, pot;
-            string classifRodagem, classifPot;
+            double km;
 
-            Console.WriteLine("informe a quantidade de carros");
-            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a quantidade de carros");
+            n = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i < n; i++)
+            for (i = 0; i < n; i++)
             {
-                Console.WriteLine("Informe o modelo");
+                Console.WriteLine("Informe o nome do carro");
                 modelo = Console.ReadLine();
 
-                Console.WriteLine("Informe a quilometragem");
+                Console.WriteLine("Informe a quilometragem do carro");
                 km = double.Parse(Console.ReadLine());
 
-                Console.WriteLine("Informe a potencia");
-                pot = double.Parse(Console.ReadLine());
+                Console.WriteLine("Informe a potencia do carro");
+                pot = int.Parse(Console.ReadLine());
 
+                Console.WriteLine(Classificar(modelo, km, pot));
 
-                if (km <= 5000)
-                    classifRodagem = "novo";
-                else if (km <= 30000)
-                    classifRodagem = "semi novo";
-                else
-                    classifRodagem = "velho";
-
-                if (pot > 200)
-                    classifPot = "potente";
-                else if (pot < 200 && pot > 120)
-                    classifPot = "forte";
-                else
-                    classifPot = "popular";
-
-                Console.WriteLine("{0} - {1} - {2}", modelo, classifRodagem, classifPot);
             }
+        }
+
+        public static string Classificar(string modelo, double km, int pot)
+        {
+            string classifRodagem, classifPot;
+
+            if (km <= 5000)
+                classifRodagem = "novo";
+
+            else if (km <= 30000)
+                classifRodagem = "seminovo";
+
+            else
+                classifRodagem = "velho";
+
+            if (pot < 120)
+                classifPot = "popular";
+
+            else if (pot <= 200)
+                classifPot = "forte";
+
+            else
+                classifPot = "potente";
+
+            return String.Format("{0} - {1} - {2}", modelo, classifRodagem, classifPot);
+
         }
     }
 }
