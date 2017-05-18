f4f0d5deb2317dbb8e202eb443180ee910ce922a
jairo151099 <jairojunior10410@gmail.com>
2017-05-18 13:45:10 -0300

    Etapa 2: Classificação de vários carros

--- Trabalhos/ExercicioRevisao/ExercicioRevisao/Program.cs
+++ Trabalhos/ExercicioRevisao/ExercicioRevisao/Program.cs
@@ -9,34 +9,42 @@ namespace ExercicioRevisao
     {
         static void Main(string[] args)
         {
+            int n;
             string modelo;
             double km, pot;
-
-            Console.WriteLine("Informe o modelo");
-            modelo = Console.ReadLine();
-
-            Console.WriteLine("Informe a quilometragem");
-            km = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Informe a potencia");
-            pot = double.Parse(Console.ReadLine());
-
             string classifRodagem, classifPot;
 
-            if (km <= 5000)
-                classifRodagem = "novo";
-            else if (km <= 30000)
-                classifRodagem = "semi novo";
-            else
-                classifRodagem = "velho";
+            Console.WriteLine("informe a quantidade de carros");
+            n = int.Parse(Console.ReadLine());
 
-            if (pot > 200)
-                classifPot = "potente";
-            else if (pot < 200 && pot > 120)
-                classifPot = "forte";
-            else
-                classifPot = "popular";
-            Console.WriteLine("{0} - {1} - {2}", modelo, classifRodagem, classifPot);
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Informe o modelo");
+                modelo = Console.ReadLine();
+
+                Console.WriteLine("Informe a quilometragem");
+                km = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Informe a potencia");
+                pot = double.Parse(Console.ReadLine());
+
+
+                if (km <= 5000)
+                    classifRodagem = "novo";
+                else if (km <= 30000)
+                    classifRodagem = "semi novo";
+                else
+                    classifRodagem = "velho";
+
+                if (pot > 200)
+                    classifPot = "potente";
+                else if (pot < 200 && pot > 120)
+                    classifPot = "forte";
+                else
+                    classifPot = "popular";
+
+                Console.WriteLine("{0} - {1} - {2}", modelo, classifRodagem, classifPot);
+            }
         }
     }
 }
