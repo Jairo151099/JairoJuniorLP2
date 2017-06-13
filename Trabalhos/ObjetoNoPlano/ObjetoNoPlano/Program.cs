e737ea8333f00f44e8df92ad753950f5a04e00b6
JairoJuniorlp2 <jairojunior10410@gmail.com>
2017-06-13 18:26:27 -0300

    Objeto no plano

--- Trabalhos/ObjetoNoPlano/ObjetoNoPlano/Program.cs
+++ Trabalhos/ObjetoNoPlano/ObjetoNoPlano/Program.cs
@@ -16,21 +16,17 @@ namespace ObjetosNoPlano
             Console.Write("Digite Y inicial: ");
             int y = int.Parse(Console.ReadLine());
 
-            /* O tipo Objeto2D não existirá até que você crie uma nova 
-             * classe para representá-lo em seu projeto!*/
+            
             Objeto2D obj = new Objeto2D();
 
-            /* Inicialização da posição do objeto */
+           
             obj.X = x;
             obj.Y = y;
 
             Console.Write("Digite comando: ");
             ConsoleKey comando = Console.ReadKey().Key;
 
-            /* O while abaixo só funcionará corretamente caso a 
-             * classe Objeto2D tenha sido construida de forma adequada, 
-             * ou seja, que saiba realizar as tarefas de andar para a direita 
-             * e de retornar uma string exibindo suas coordenadas */
+           
             while (comando != ConsoleKey.Escape)
             {
                 if (comando == ConsoleKey.RightArrow)
