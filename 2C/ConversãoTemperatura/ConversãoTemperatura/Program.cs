802044488643e3594a009af4eacec036c7e1e5ec
JairoJuniorlp2 <jairojunior10410@gmail.com>
2017-08-08 22:52:07 -0300

    atual

---
+++ 2C/ConversãoTemperatura/ConversãoTemperatura/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            TemperaturaCelsius a = new TemperaturaCelsius();
+            while (true)
+            {
+                Console.WriteLine("1 - Nova Temperatura");
+                Console.WriteLine("2 - Para Fahrenheit");
+                Console.WriteLine("3 - Para Kelvin");
+                Console.WriteLine("0 - Sair");
+                x = int.Parse(Console.ReadLine());
+                if (x == 1)
+                {
+                    a.valor = double.Parse(Console.ReadLine());
+                }
+                if (x == 2)
+                {
+                    Console.WriteLine(a.convertF());
+                }
+                if (x == 3)
+                {
+                    Console.WriteLine(a.convertK());
+                }
+                if (x == 0)
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
---
+++ 2C/ConversãoTemperatura/ConversãoTemperatura/TemperaturaCelsius.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class TemperaturaCelsius
+    {
+        public double valor;
+        public double convertF()
+        {
+            return valor * 1.8 + 32;
+        }
+        public double convertK()
+        {
+            return valor + 273;
+        }
+    }
+}
