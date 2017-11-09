7baa175bf65a862051afbca64e70a19614f1dee6
Miguel Amoedo <miguel.amoedo.cardoso@gmail.com>
2017-11-09 14:23:08 -0200

    SQL BASE

---
+++ SQLBASE/SQLBASE/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLBASE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SqlConnection conexao = new SqlConnection("Server=localhost;Database=escola;Uid=root;Pwd=");
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = conexao;
+            cmd.CommandText = "INSERT INTO PROFESSOR(Nome, Siape, Salario) VALUES ('Pedro', '123789', 20000);";
+
+            conexao.Open();
+            cmd.ExecuteNonQuery();
+            conexao.Close();
+
+            Console.WriteLine("OK");
+                    
+        }
+    }
+}
