54b5403d9ef9e83ef609ed575769be5bbb39f2b9
Sarah <sarahborretti@gmail.com>
2018-06-28 10:27:00 -0300

    ETAPA 4

---
+++ Veículo_Ex1/Veículo_Ex1/Carro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veículo_Ex1
+{
+    class Carro
+    {
+        
+            public string Modelo {get; set;}
+            public double Quilometragem {get; set;}
+            public int Potencia {get; set;}
+        }
+    }
+
+    
+
